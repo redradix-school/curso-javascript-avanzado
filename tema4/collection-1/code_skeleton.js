c3399083eeb4708dd8efc410d88a569d1350ed5f
Miguel <miguel@redradix.com>
2016-01-21 15:27:04 +0100

    collection skeleton

---
+++ tema4/collection-1/code_skeleton.js
@@ -0,0 +1,30 @@
+// Modelo
+
+var Producto = ProJS.Model.extend({
+  init: function() {
+    this._super.apply(this, arguments);
+  },
+  defaults: {
+    nombre: "Producto sin nombre",
+    categoria: "Sin categorizar",
+    pais: "España",
+    precio: 0
+  },
+  set: function(attrs, options) {
+    return this._super(attrs, merge(options, {validate: true}));
+  },
+  validate: function(attrs) {
+    var vacio = /^\s*$/,
+        pais = /España|Francia|Portugal/,
+        numerico = /^\d+$/;
+    if (vacio.test(attrs["nombre"]) ||
+        vacio.test(attrs["categoria"]) ||
+        (attrs["pais"] && !pais.test(attrs["pais"])) ||
+        (attrs["precio"] && !numerico.test(attrs["precio"])) ||
+        (attrs["id"] && !numerico.test(attrs["id"]))) {
+      return "Producto no válido!";
+    }
+  }
+});
+
+// Colección
